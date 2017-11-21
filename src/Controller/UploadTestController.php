e6ad89db230069f1982a47aba8916d050e4245ad
Felds Liscia <dev@felds.com.br>
2017-11-21 00:54:52 -0200

    upload files to S3

--- src/Controller/UploadTestController.php
+++ src/Controller/UploadTestController.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Config;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UploadTestController extends Controller
@@ -12,9 +16,22 @@ class UploadTestController extends Controller
     /**
      * @Route("test-upload")
      */
-    public function __invoke(S3Client $fs)
+    public function __invoke(Request $request, AwsS3Adapter $adapter)
     {
-        dump($fs);
-        die;
+        $config = new Config();
+
+        $file = $request->files->get('upload');
+        if ($file instanceof UploadedFile && $file->isValid()) {
+            $stream = fopen($file->getRealPath(), 'r+');
+            $adapter->writeStream($file->getClientOriginalName(), $stream, $config);
+            fclose($stream);
+        }
+
+        return new Response('
+            <form method="POST" enctype="multipart/form-data">
+                <input type="file" name="upload">
+                <button type="submit">Up and away!</button>
+            </form>
+        ');
     }
 }
